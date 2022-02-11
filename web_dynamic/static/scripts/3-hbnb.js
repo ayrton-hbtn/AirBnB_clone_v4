7aca3c69a3cda6dd6f21392ac78a8b2bc825403a
Andy <3283@holbertonschoool.com>
2022-02-11 19:52:51 +0000

    Updated script

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -33,10 +33,25 @@ $(document).ready(function () {
     type: 'POST',
     url: 'http://0.0.0.0:5001/api/v1/places_search',
 	contentType: "application/json",
-	data: "{}", 
-	dataType: 'json',
-	success: function (place) {	
-		console.log(place);
-	}
+	data: "{}",
+	success: function (placesResponse) {
+		for (let placeItem of placesResponse) {
+			$( ´�<article>
+				<div class="title_box">
+					<h2>$(placeItem.name)</h2>
+					<div class="price_by_night">$$(placeItem.price_by_night)</div>
+				</div>
+				<div class="information">
+					<div class="max_guests">$(placeItem.max_guest) $((placeItem.max_guest == 1 ) ? "Guest" : "Guests";) </div>
+					<div class="number_rooms">$(placeItem.number_rooms) $((placeItem.number_rooms == 1 ) ? "Bedroom" : "Bedrooms";)</div>
+					<div class"number_bathrooms">$(placeItem.number_bathrooms) $((placeItem.number_bathrooms == 1 ) ? "Bathroom" : "Bathrooms";)</div>
+				</div>
+				<div class="user">
+					<b>Owner:</b>&(placeItem.user.first_name) $(placeItem.user.last_name)
+				</div>
+				<div> class="description">
+				$(placeItem.description)
+				</div>
+				</article>´ ).appendTo( $("section.places") );
   });
 });
